8eb005fc45236d5d31cb497e30079c74cee2aa4f
Ángela Enríquez Gómez <anenriqu@outlook.com>
2019-12-08 12:46:05 +0100

    Rename test to get_path. Return path, mean and variance

---
+++ get_path.py
@@ -0,0 +1,20 @@
+import argparse
+
+from planner.planner import Planner
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('source', type=str, help='Name of source node')
+    parser.add_argument('destination', type=str, help='Name of destination node')
+    args = parser.parse_args()
+
+    planner = Planner()
+    planner.load_map('planner/maps/brsu.json')
+
+    path = planner.get_path(args.source, args.destination)
+
+    print("Path from {} to {}: {}".format(args.source, args.destination, path))
+
+    mean, variance = planner.get_estimated_duration(path)
+    print("mean: ", mean)
+    print("variance: ", variance)
