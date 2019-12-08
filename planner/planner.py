fe175507f96399dcadcf8a3ea34e0793091797b4
Ángela Enríquez Gómez <anenriqu@outlook.com>
2019-12-08 12:43:21 +0100

    planner: Add map_graph attr instance of MapGraph cls
    Includes interfaces to generate and load a map using the MapGraph instance

--- planner/planner.py
+++ planner/planner.py
@@ -1,77 +1,19 @@
-import json
-import os
-
 import networkx as nx
 import numpy as np
+from planner.map_graph import MapGraph
 from planner.utils.utils import load_yaml
-from planner.edge import Edge
 
 
 class Planner:
-    def __init__(self, map_file, edge_info_path, min_n_runs, obstacle_interval):
+    def __init__(self):
+        self.map_graph = MapGraph()
+
+    def generate_map(self, map_file, edge_info_path, min_n_runs, obstacle_interval):
         map_info = load_yaml(map_file)
-        self.min_n_runs = min_n_runs
-        self.obstacle_interval = obstacle_interval
-        self.map_graph = self.generate_map(map_info,
-                                           edge_info_path)
+        self.map_graph.generate_map(map_info, edge_info_path, min_n_runs, obstacle_interval)
 
-    def generate_map(self, map_info, edge_info_path):
-        nodes = map_info.get('nodes')
-        edges = map_info.get('edges')
-        lane_connections = map_info.get('lane-connections')
-        map_graph = nx.Graph()
-
-        for edge in edges:
-            if edge in lane_connections:
-                map_graph = self.add_connection_lane(map_graph, edge, nodes)
-            else:
-                # Get info for edge in both directions
-                undirected_edge_info = None
-                edge_info_1 = self.get_edge_info(edge[0] + '_to_' + edge[1], edge_info_path)
-                edge_info_2 = self.get_edge_info(edge[1] + '_to_' + edge[0], edge_info_path)
-                if edge_info_1 and edge_info_2:
-                    undirected_edge_info = edge_info_1 + edge_info_2
-                elif edge_info_1 and edge_info_2 is None:
-                    undirected_edge_info = edge_info_1
-                elif edge_info_2 and edge_info_1 is None:
-                    undirected_edge_info = edge_info_2
-
-                if undirected_edge_info:
-                    map_graph = self.add_edge(map_graph, edge, nodes, undirected_edge_info)
-
-        return map_graph
-
-    @staticmethod
-    def add_connection_lane(map_graph, edge, nodes):
-        map_graph.add_edge(edge[0], edge[1], connection_lane=True)
-        for node in edge:
-            map_graph.add_node(node, pose=nodes[node])
-        return map_graph
-
-    @staticmethod
-    def get_edge_info(edge_name, edge_info_path):
-        file_path = edge_info_path + edge_name + '.summary'
-        if os.path.isfile(file_path):
-            with open(file_path, 'r') as source_file:
-                for line in source_file.readlines():
-                    n_runs, mean, variance, n_obstacles = line.split(' ')
-                    return Edge(edge_name, int(n_runs), float(mean), float(variance), int(n_obstacles))
-
-    def add_edge(self, map_graph, edge, nodes, edge_info):
-        if edge_info.max_n_obstacles in self.obstacle_interval \
-                and edge_info.n_runs >= self.min_n_runs:
-
-            # If edge already exists combine this edge_info with the previous one
-            if map_graph.has_edge(edge[0], edge[1]):
-                edge_previous_info = map_graph.get_edge_data(edge[0], edge[1])
-                edge_info = Edge.from_dict(edge_previous_info) + edge_info
-
-            map_graph.add_edge(edge[0], edge[1], **edge_info.to_dict())
-
-            for node in edge:
-                map_graph.add_node(node, pose=nodes[node])
-
-        return map_graph
+    def load_map(self, json_file):
+        self.map_graph = self.map_graph.from_json(json_file)
 
     def distance(self, node_1, node_2):
         x1, y1, z1 = self.map_graph.nodes[node_1]['pose']
@@ -116,8 +58,3 @@ class Planner:
 
         return mean, variance
 
-    def to_json(self, file_path):
-        data = nx.node_link_data(self.map_graph)
-        with open(file_path, 'w') as outfile:
-            json.dump(data, outfile, indent=2)
-        return data
