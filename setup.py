3a7c0ba8018d3d72cb990b6c0857055e9ccaa1f0
Ángela Enríquez Gómez <anenriqu@outlook.com>
2019-12-11 10:00:08 +0100

    setup: Add packages and install_requires

--- setup.py
+++ setup.py
@@ -3,8 +3,15 @@
 from setuptools import setup
 
 setup(name='planner',
-      packages=['planner', 'planner.utils'],
+      packages=['planner', 'planner.maps', 'planner.utils'],
       version='0.1.0',
+      install_requires=[
+            'networkx',
+            'pyYAML',
+            'numpy',
+            'matplotlib',
+            'importlib_resources'
+      ],
       description='Planner for experiments with MRTA',
       author='Angela Enriquez Gomez',
       author_email='angela.enriquez@smail.inf.h-brs.de',
