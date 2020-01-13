2656869a5134c94be2c67dd59a0fca71bf1b6a03
Ángela Enríquez Gómez <anenriqu@outlook.com>
2020-01-13 08:26:07 +0100

    Generate map without obstacles

--- generate_map.py
+++ generate_map.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     map_file = 'planner/config/brsu.yaml'
     edge_info_path = '../ropod_rosbag_processing/ropod_rosbag_processing/angela/results/'
     min_n_runs = 1
-    obstacle_interval = list(range(0, 3))
+    obstacle_interval = [0]
 
     planner = Planner('brsu', load_map=False)
     planner.generate_map(map_file, edge_info_path, min_n_runs, obstacle_interval)
